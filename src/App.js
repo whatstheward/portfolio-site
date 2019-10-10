620740fd68ae4bf48e339a7ec4e2718796961528
github username <travisemerymartin@gmail.com>
2019-10-10 19:18:02 -0400

    basic setup complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './index.css';
+import background from './HeaderImage.jpg';
 
 function App() {
+  const styleObj = {
+    color: 'red',
+    border:'5px solid black',
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh',
+    minWidth: '100vw'
+  };
+
   return (
-    <div className="App">
+    <div className='App' style={styleObj}>
+      THIS IS TEXT ON THE DOM
     </div>
   );
 }
