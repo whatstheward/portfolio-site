b3f865ea8c129423403edde6198b5b5111385dc9
Maddie <whatstheward@gmail.com>
2019-10-11 13:55:44 -0400

    add App.css file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import './index.css';
+import './App.css';
 import background from './HeaderImage.jpg';
 
 function App() {
-  const styleObj = {
-    color: 'red',
-    border:'5px solid black',
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    minHeight: '100vh',
-    minWidth: '100vw'
-  };
+
 
   return (
-    <div className='App' style={styleObj}>
+    <div className='App'>
       THIS IS TEXT ON THE DOM
     </div>
   );
